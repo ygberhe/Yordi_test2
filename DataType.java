f17d96128f4873f79d12b5d4dd3f049e12ddddfe
ygberhe <gebruyordanos789@gmail.com>
2025-03-04 19:52:44 -0500

    i added four files

---
+++ DataType.java
@@ -0,0 +1,17 @@
+package chapter2;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+System.out.println("craft");
+System.out.println(13);
+System.out.println(true);
+System.out.println(false);
+System.out.println(0.099888768);
+System.out.println('M');
+System.out.println("student@craft.com");
+	}
+
+}
